2319e21edc230acc159eb69cfcea1a432c9736ca
Anujan <anujancis@gmail.com>
2020-04-16 21:12:07 +0800

    View Activity

---
+++ app/src/main/java/com/example/clean_city/AcceptView.java
@@ -0,0 +1,13 @@
+package com.example.clean_city;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcceptView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_view);
+    }
+}
---
+++ app/src/main/java/com/example/clean_city/ProfileOfClient.java
@@ -0,0 +1,14 @@
+package com.example.clean_city;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileOfClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_of_client);
+    }
+}
